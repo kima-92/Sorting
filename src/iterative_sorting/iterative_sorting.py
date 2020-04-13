6495f1f8026186e607875b91d68867526220e5a9
Wilma <kima_92@fastmail.com>
2020-04-13 18:59:38 -0400

    Completed bubble_sort function

--- src/iterative_sorting/iterative_sorting.py
+++ src/iterative_sorting/iterative_sorting.py
@@ -21,8 +21,40 @@ def selection_sort( arr ):
 # TO-DO:  implement the Bubble Sort function below
 def bubble_sort( arr ):
 
+    # set didSwap to True
+    did_swap = True
+
+    # While didSwap == True
+    while did_swap == True:
+        # grab arr[0]
+        for i in range(0, len(arr)):
+            print(f"Grabed index {i} in outer loop")
+            
+            # grab arr[1]
+            for j in range(i+1, len(arr)):
+                print(f"Grabed index {j} in inner loop")
+
+                # If arr[1] > arr[0]
+                if arr[j] < arr[i]:
+                    # Swap them
+                    arr[j], arr[i] = arr[i], arr[j]
+                    # Set didSwap to True
+                    did_swap = True
+                    print(f"Swapped {arr[i]} with {arr[j]} and set did_swap to True")
+                
+                # Else:
+                else:
+                    # Don't swap them 
+                    # Set didSwap to False
+                    did_swap = False
+                    print(f"Did not swap {arr[i]} with {arr[j]}, set did_swap to False")
+
     return arr
 
+a = [9, 1, 8, 4, 3, 5, 6, 10, 7, 2]
+print(f"\nOLD list: {a}")
+new_list = bubble_sort(a)
+print(f"NEW list: {new_list}")
 
 # STRETCH: implement the Count Sort function below
 def count_sort( arr, maximum=-1 ):
