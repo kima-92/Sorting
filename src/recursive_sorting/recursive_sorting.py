b7ea976cef75f9163f522cbfe03e1ed37f68ebff
Wilma <kima_92@fastmail.com>
2020-04-15 10:16:04 -0400

    Manage_sort function works, without the helper funtion. Merge function still in progress

--- src/recursive_sorting/recursive_sorting.py
+++ src/recursive_sorting/recursive_sorting.py
@@ -16,8 +16,6 @@ Merge Sort   ( Divide & Conquer )
 
 
 
-
-
 # Helper function to merge 2 sorted arrays
 def merge(left_l, right_l):
     # Set index value for the list to the left, and the list to the right
@@ -66,53 +64,10 @@ def merge(left_l, right_l):
         x += 1
 
     return merged_l
-        
-        
-    """
-    elements = len( arrA ) + len( arrB )
-    merged_arr = [0] * elements
-    print(f"elements: {elements}")
-    print(f"merge_arr: {merged_arr}")
-
-    # Get the length of each array
-    list_length = len(arrA) + len(arrB)
-    print(f"list_length: {elements}")
-    # Set new_array
-    new_array = []
-    # For i in len(list_length):
-    for x in range(0, list_length):
-        # Compare item in arrA[0] with arrB[0]
-        print(f"x: {x}")
-        for i in arrA:
-            print(f"i: {i}")
-            for j in arrB:
-                print(f"j: {j}")
-                # If arrA[0] is smaller than arrB[0]
-                if i < j:
-                    print(f"compared i: {i} and j: {j}")
-                    # append arrA[0] to new_array
-                    new_array.append(i)
-                    print(f"Appended i")
-
-                    arrA.remove(i)
-                    print(f"removed {i} from arrA")
-                # Else
-                else:
-                    # Append arrB[0] to new_array
-                    new_array.append(j)
-                    print("appanded j")
-                    arrB.remove(j)
-                    print(f"removed {j} from arrB")
-                    """
-    
-    #return new_array
-
 
 # Implement the Merge Sort function below USING RECURSION
 def merge_sort( arr ):
     # Devide the list into individial lists ( A list of lists? )
-
-    sorted_list = []
     
     # Base case
     if len(arr) > 1: 
@@ -126,13 +81,51 @@ def merge_sort( arr ):
         # For every two lists, use the merge function
         merge_sort(left_l)  # By the time this gets done, it will be a sorted list
         merge_sort(right_l) # By the time this gets done, it will be a sorted list
+        
+        # Set index value for the list to the left, and the list to the right
+        i = 0   # Current index in left_l
+        j = 0   # Current index in right_l
+        
+        # Set index for the merged_l, to insert the new values
+        x = 0
+        
+        # Compare left_l[i] with righ_l[j]
+        # While i is less than the length of left_l AND j is less than the length of right_l
+        while i < len(left_l) and j < len(right_l):
+            
+            # if left_l[0] is smaller than right_l[0]
+            if left_l[i] < right_l[j]:
+                # Set merged_l[0] as left_l[0]
+                arr[x] = left_l[i]
+                
+                # Increment index of both left_l and merged_l
+                i += 1      # So we ignore the previous index from now on
+                x += 1      # So we can add items to next index
 
-        # Merge the separated lists
-        sorted_list = merge(left_l, right_l)
-        print(f"Merged a list: {sorted_list}")
+            # else
+            else:
+                # Set arr[0] as right_l[0]
+                arr[x] = right_l[j]
+
+                # Increment index of both right_l and merged_l
+                j += 1      # So we ignore the previous index from now on
+                x += 1      # So we can add items to next index
+
+        # Make sure we add whatever items are left in each array
+        while i < len(left_l):
+            arr[x] = left_l[i]
+        
+            i += 1
+            x += 1
+
+        while j < len(right_l):
+            arr[x] = right_l[j]
+        
+            j += 1
+            x += 1
 
 
-    return sorted_list
+    return arr
 
 # Testing merge_sort
 
